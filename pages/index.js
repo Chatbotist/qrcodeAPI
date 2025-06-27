62b8d0afa315e19d397036b3b327e90f15fab3be
Дмитрий <yurchuk.dima@inbox.ru>
2025-06-27 06:07:56 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,6 @@ export default function Home() {
   return (
     <div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
       <h1>Генератор QR-кодов</h1>
-      <p>Отправьте POST запрос на <code>/api/generate-qr</code> с JSON телом:</p>
-      <pre>{`{
-  "text": "Ваш текст"
-}`}</pre>
-      <p>В ответе получите Data URL QR-кода, который можно использовать в теге img:</p>
-      <pre>{`{
-  "qrDataUrl": "data:image/png;base64,..."
-}`}</pre>
     </div>
   );
 }
